4c9ac5e369699c8a6411beeae3a0c0f5abdf7b90
Jad Millus <91867446+Jadmillus@users.noreply.github.com>
2022-12-11 04:46:58 +0300

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,11 @@ int main(void)
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
-        {
-	  	putchar(d);
+	{
+		putchar(d);
 		if (d != '9')
-	        {
-		  	putchar(',');
+		{
+			putchar(',');
 			putchar(' ');
 		}
 	}
