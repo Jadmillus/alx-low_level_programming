bb6223ed2cfd00f30a461e8f0a21ebab22c336d8
Jad Millus <91867446+Jadmillus@users.noreply.github.com>
2022-12-11 03:38:03 +0300

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -16,14 +16,11 @@ int main(void)
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-        {
-	  	if (low != e && low != q)
+	{
+		if (low != e && low != q)
 			putchar(low);
-        }
+	}
 	putchar('\n');
 
 	return (0);
 }
-
-	
-	
